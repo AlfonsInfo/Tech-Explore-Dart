bbfaef9d2d57e0c6a02c9f736c5f6ab3a741b683
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-02-20 12:08:38 +0700

    first commit branch 3

---
+++ bin/dart_learn.dart
@@ -0,0 +1,5 @@
+import 'package:dart_learn/dart_learn.dart' as dart_learn;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_learn.calculate()}!');
+}
---
+++ test/dart_learn_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_learn/dart_learn.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
